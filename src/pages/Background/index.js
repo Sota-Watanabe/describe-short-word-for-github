82f692695f1fd52aa3ef8bc8ef26e72f87f17b9c
Sota-Watanabe <stronger.riceball@gmail.com>
2022-08-13 16:50:10 +0900

    fix: 環境の修正 github.com

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,7 +1,7 @@
 chrome.tabs.onUpdated.addListener(function (tabId, info, tab) {
   if (
     info.status === 'complete' &&
-    tab.url.indexOf('https://jp.mercari.com') !== -1
+    tab.url.indexOf('https://github.com') !== -1
   ) {
     chrome.scripting.executeScript({
       target: { tabId: tab.id, allFrames: true },
