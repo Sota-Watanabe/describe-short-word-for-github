8dfc6c4ea6dddaaac2f8dbddc5e6791b233f1ff6
Sota-Watanabe <stronger.riceball@gmail.com>
2022-08-20 00:50:35 +0900

    外部ファイルを読み込むよう修正

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,19 +1,21 @@
-const searchShortWord = (paragraph) => {
-  const SearchString = '(WIP)';
-  const RegularExp = new RegExp(SearchString, 'g');
-  const ReplaceString =
-    '<span class="wip">$1</span><span>(Work In Progress, 対応中)</span>';
-  const ResString = paragraph.replace(RegularExp, ReplaceString);
-  console.log(`ResString=${ResString}`);
-  return ResString;
+import { shordWordDictionary } from './modules/shordWordDictionary';
+
+const setClassForDescription = (paragraph) => {
+  let updatedParagraph = paragraph;
+  for (const shortWord in shordWordDictionary) {
+    const searchString = `(${shortWord})`;
+    const RegularExp = new RegExp(searchString, 'g');
+    const ReplaceString = `<span class="short-word">$1</span><span>(${shordWordDictionary[shortWord].full}: ${shordWordDictionary[shortWord].description})</span>`;
+    updatedParagraph = updatedParagraph.replace(RegularExp, ReplaceString);
+  }
+  return updatedParagraph;
 };
 
 let paragraph_list = document.querySelectorAll('.comment-body > p');
-console.log(JSON.stringify(paragraph_list));
 for (const paragraph of paragraph_list) {
   // 特定クラスが含まれていない場合
   if (!paragraph.classList.contains('done-replace-with-description')) {
-    paragraph.innerHTML = searchShortWord(paragraph.innerHTML);
+    paragraph.innerHTML = setClassForDescription(paragraph.innerHTML);
     paragraph.classList.add('done-replace-with-description');
   }
 }
---
+++ src/pages/Content/modules/shordWordDictionary.js
@@ -0,0 +1,14 @@
+export const shordWordDictionary = {
+  WIP: { full: 'Work In Progress', description: '対応中' },
+  wip: { full: 'Work In Progress', description: '対応中' },
+  FYI: { full: 'For Your Information', description: 'ご参考までに' },
+  fyi: { full: 'For Your Information', description: 'ご参考までに' },
+  IMO: { full: 'In My Opiniton', description: '個人的な意見ですが' },
+  imo: { full: 'In My Opiniton', description: '個人的な意見ですが' },
+  IMHO: { full: 'In My Humble Opiniton', description: '個人的な意見ですが' },
+  imho: { full: 'In My Humble Opiniton', description: '個人的な意見ですが' },
+  LGTM: { full: 'Looks Good To Me', description: '自分的にはOKです' },
+  lgtm: { full: 'Looks Good To Me', description: '自分的にはOKです' },
+  NITS: { full: 'nits pick', description: '細かい話だけど' },
+  nits: { full: 'nits pick', description: '細かい話だけど' },
+};
